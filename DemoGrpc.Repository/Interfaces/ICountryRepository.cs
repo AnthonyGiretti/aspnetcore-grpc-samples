fec3370cfc1b2efdfd5351536cd8d88d3ace4208
Anthony Giretti <anthony.giretti@gmail.com>
2019-12-07 20:35:08 -0500

    add reposistory interface

---
+++ DemoGrpc.Repository/Interfaces/ICountryRepository.cs
@@ -0,0 +1,15 @@
+﻿using DemoGrpc.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DemoGrpc.Repository.Interfaces
+{
+    public interface ICountryRepository
+    {
+        Task<List<Country>> GetAsync();
+        Task<Country> GetByIdAsync(int countryId);
+        Task<long> AddAsync(Country country);
+        Task<int> UpdateAsync(int countryId, Country country);
+        Task<int> DeleteAsync(int countryId);
+    }
+}
