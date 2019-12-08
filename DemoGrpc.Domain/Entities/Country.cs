7b6c1062e49418fd08ae39564064fd842f766507
Anthony Giretti <anthony.giretti@gmail.com>
2019-12-07 20:27:12 -0500

    adding dbcontext + domain layer

---
+++ DemoGrpc.Domain/Entities/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoGrpc.Domain.Entities
+{
+    public class Country
+    {
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DemoGrpc.Repository/Database/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using DemoGrpc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoGrpc.Repository.Database
+{
+    public class DemoDbContext : DbContext
+    {
+        public DemoDbContext(DbContextOptions<DemoDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Country> Country { get; set; }
+    }
+}
