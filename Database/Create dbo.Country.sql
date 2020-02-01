8ab006aa9b70c4e7bdb97908ba05676f9b9eca9a
Anthony Giretti <anthony.giretti@gmail.com>
2020-01-31 23:42:30 -0500

    add sql file

---
+++ Database/Create dbo.Country.sql
@@ -0,0 +1,17 @@
+﻿USE [DemoOrmLite]
+GO
+
+/****** Object: Table [dbo].[Country] Script Date: 1/31/2020 11:38:02 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Country] (
+    [CountryId]   INT          IDENTITY (1, 1) NOT NULL,
+    [CountryName] VARCHAR (50) NOT NULL,
+    [Description] VARCHAR (50) NULL
+);
+
+
---
+++ Database/dbo.Country.data.sql
@@ -0,0 +1,6 @@
+SET IDENTITY_INSERT [dbo].[Country] ON
+INSERT INTO [dbo].[Country] ([CountryId], [CountryName], [Description]) VALUES (1, N'France', N'beautiful and not so cold!')
+INSERT INTO [dbo].[Country] ([CountryId], [CountryName], [Description]) VALUES (2, N'Canada', N'beautiful and very cold')
+INSERT INTO [dbo].[Country] ([CountryId], [CountryName], [Description]) VALUES (1003, N'Australia', N'incredible wildlife')
+INSERT INTO [dbo].[Country] ([CountryId], [CountryName], [Description]) VALUES (4003, N'Japan', N'rising sun country')
+SET IDENTITY_INSERT [dbo].[Country] OFF
